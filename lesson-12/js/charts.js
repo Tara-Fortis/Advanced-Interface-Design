6f3484d71ad79f7ff5f93f40f80ccf866e813663
Tineil_A <tine6573@gmail.com>
2025-11-25 13:36:07 -0500

    end of lesson 12

--- lesson-12/js/charts.js
+++ lesson-12/js/charts.js
@@ -1,27 +1,40 @@
 // STEP 11: Get length of path for circle (circumference)
-
+let path = document.querySelector("svg #pieWedge");
+let length = path.getTotalLength(); // circumference!!!
+console.log(path);
 
 // STEP 12: Set the length of the space between dashes to the full circumference
-
+path.setAttribute("stroke-dasharray", "0 1" + length);
 
 // STEP 13: Dynamically set the width of the pie segment to reflect the text value
 // STEP 13a: Capture value of % inside the pie chart SVG text element
-
+let percent = document.querySelector("svg text").textContent;
 // STEP 13b: Remove the % sign from the text value
-
+percent = percent.replaceAll("%", "");
+percent = parseInt(percent, 10);
 // STEP 13c: convert the string to an integer
-
+percent = percent / 100;
+console.log(percent);
 // STEP 13d: Calculate width of pie segment as a percentage of the circumference of the circle - this will be used for the length of the dash part of the stroke
-
-
+let pieWidth = percent * length;
+console.log(pieWidth);
 // STEP 14: Grab the external CSS as one of the document object properties
-
+const cssRulesList = document.styleSheets[0].cssRules;
+console.log(cssRulesList);
 
 // STEP 15: Loop through the CSS rules to capture the one that controls the :active state for the circle
-
+// let svgActiveRule;
+// for (let i = 0; i < cssRulesList.length; i++) {
+//     if (cssRulesList[i].selectorText == "svg:active #pieWedge ") {
+//         // found the rule we want!!
+//         console.log("Yay!");
+//         svgActiveRule = cssRulesList[i];
+//     }
+// }
 
 // STEP 16: Build the string for the new CSS property value
-
+// let propertyValue = pieWidth + " " + length;
+// console.log(propertyValue);
 
 // STEP 17: Update the CSS declaration to reflect the new property value for stroke-dasharray
-
+// svgActiveRule.style.setProperty("stroke-dasharray", propertyValue);
